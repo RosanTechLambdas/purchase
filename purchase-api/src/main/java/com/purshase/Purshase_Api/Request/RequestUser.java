01b10512008100641b47c7d3ee8794f454b10ef2
RosanTechLambdas <rosan@techlambdas.com>
2025-05-20 10:48:10 +0530

    Delete purchase-api directory

--- purchase-api/src/main/java/com/purshase/Purshase_Api/Request/RequestUser.java
+++
@@ -1,9 +0,0 @@
-package com.purshase.Purshase_Api.Request;
-
-import lombok.Data;
-
-@Data
-public class RequestUser {
-    private String userName;
-    private String password;
-}
--- purchase-api/src/main/java/com/purshase/Purshase_Api/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.purshase.Purshase_Api.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String s) {
-        super(s);
-    }
-}
